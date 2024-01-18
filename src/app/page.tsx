38a425c9a633455aa8d51ac6c43121c63668da1a
dillonzellis <dillonzellis@gmail.com>
2024-01-17 20:40:00 -0500

    feat: rough hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { NavBar } from "@/components/nav/Navbar";
+
 export default function Home() {
-  return <main className="">yeppers</main>;
+  return (
+    <main className="">
+      <div>
+        <Image
+          src="/home-hero-placeholder.jpg"
+          alt="hero"
+          width={1280}
+          height={835}
+        />
+        <div>
+          <NavBar />
+        </div>
+      </div>
+    </main>
+  );
 }
