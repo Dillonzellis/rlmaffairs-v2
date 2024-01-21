1c72c1cd998629e83a71574b101c4f51ad42f2e4
dillonzellis <dillonzellis@gmail.com>
2024-01-21 11:07:02 -0500

    style: add paper bg and default text colors to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
+import { cn } from "@/lib/utils";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -17,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body
+        className={cn(
+          "bg-slate-50 bg-[url('/paper-bg-min.png')] text-slate-950",
+          inter.className,
+        )}
+      >
         {children}
         <Footer />
       </body>
